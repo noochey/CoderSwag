7053dd7020f30400e1fd5e1fd124e8458c2cafb3
nibreining <nibreining@dagdasolutions.de>
2018-04-17 20:56:20 +0200

    finish

--- app/src/main/java/com/selfhack/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/selfhack/coderswag/controller/ProductsActivity.kt
@@ -1,16 +1,48 @@
 package com.selfhack.coderswag.controller
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import com.selfhack.coderswag.R
+import com.selfhack.coderswag.adapters.CategoryRecycleAdapter
+import com.selfhack.coderswag.adapters.ProductsAdapter
+import com.selfhack.coderswag.services.DataService
 import com.selfhack.coderswag.utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType)) { product ->
+
+        }
+
+        productListView.adapter = adapter
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
+        productListView.setHasFixedSize(true)
     }
 }
--- app/src/main/java/com/selfhack/coderswag/services/DataService.kt
+++ app/src/main/java/com/selfhack/coderswag/services/DataService.kt
@@ -7,31 +7,42 @@ import java.util.*
 object DataService {
 
     val categories = listOf<Category>(
-            Category("SHIRTS","shirtimage"),
+            Category("SHIRTS", "shirtimage"),
             Category("HOODIES", "hoodieimage"),
             Category("HATS", "hatimage"),
             Category("DIGITAL", "digitalgoodsimage")
     )
 
     val hats = listOf<Product>(
-            Product("Devslopes Grapginc Beanie","18$", "hat01"),
-            Product("Devslopes Hat Black","20$", "hat01"),
-            Product("Devslopes Hat White","20$", "hat01"),
-            Product("Devslopes Hat Blue","15$", "hat01")
+            Product("Devslopes Grapginc Beanie", "18$", "hat1"),
+            Product("Devslopes Hat Black", "20$", "hat1"),
+            Product("Devslopes Hat White", "20$", "hat1"),
+            Product("Devslopes Hat Blue", "15$", "hat1")
     )
 
     val hoodies = listOf<Product>(
-            Product("Devslopes Hoodi Gray","28$", "hoodie01"),
-            Product("Devslopes Hoodi Black","32$", "hoodie02"),
-            Product("Devslopes Hoodi Blue","31$", "hoodie03"),
-            Product("Devslopes Hoodi White","30$", "hoodie04")
+            Product("Devslopes Hoodi Gray", "28$", "hoodie1"),
+            Product("Devslopes Hoodi Black", "32$", "hoodie2"),
+            Product("Devslopes Hoodi Blue", "31$", "hoodie3"),
+            Product("Devslopes Hoodi White", "30$", "hoodie4")
     )
 
     val shirts = listOf<Product>(
-            Product("Devslopes Shirt Gray","18$", "shirt01"),
-            Product("Devslopes Shirt Black","22$", "shirt02"),
-            Product("Devslopes Shirt Blue","21$", "shirt03"),
-            Product("Devslopes Shirt White","20$", "shirt04"),
-            Product("Devslopes Shirt Studio","20$", "shirt05")
+            Product("Devslopes Shirt Gray", "18$", "shirt1"),
+            Product("Devslopes Shirt Black", "22$", "shirt2"),
+            Product("Devslopes Shirt Blue", "21$", "shirt3"),
+            Product("Devslopes Shirt White", "20$", "shirt4"),
+            Product("Devslopes Shirt Studio", "20$", "shirt5")
     )
+
+    val digitalGoods = listOf<Product>()
+
+    fun getProducts(category: String): List<Product> {
+        return when (category) {
+            "SHIRTS" ->  shirts
+            "HATS" ->  hats
+            "HOODIES" ->  hoodies
+            else -> digitalGoods
+        }
+    }
 }
